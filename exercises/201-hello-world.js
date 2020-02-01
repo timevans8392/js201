938d8d9ecefcee3f1112ec8d7db0c47fd6fa36ca
Ashley S. Patterson <ashleyspatterson@Ashleys-MacBook-Pro.local>
2020-02-01 11:51:25 -0600

    finish the first part

--- exercises/201-hello-world.js
+++ exercises/201-hello-world.js
@@ -4,8 +4,15 @@
 // Hey Flex Class, it'd s Patrick
 // Great question
 
+function hello (name) {
+    return 'Hello, ' + name + '!'
+}
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "helloDefault" such that if no name is given it will return
 // 'Hello, world!'
 // Otherwise it behaves the same as the "hello" function.
+
+function helloDefault () { 
+
+}
